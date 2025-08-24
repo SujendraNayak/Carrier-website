ee12a6ecd2f48201ae28651e823f6dbe60e066af
SujendraNayak <sujinayak008@gmail.com>
2025-08-24 16:06:41 +0530

    `Added API endpoint to list jobs and modified job item template to include apply button and job location.`

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app=Flask(__name__)
 JOBS = [
     {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'New York, NY',
+        'salary': '$120,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'San Francisco, CA',
+        'salary': '$150,000'
+    },
+    {
+        'id': 3,
+        'title': 'Product Manager',
+        'location': 'Austin, TX',
+        'salary': '$130,000'
+    },
+      {
         'id': 1,
         'title': 'Software Engineer',
         'location': 'New York, NY',
@@ -23,6 +41,10 @@ JOBS = [
 @app.route('/')
 def home():
     return render_template('home.html',JOBS=JOBS)
+
+@app.route('/api/JOBS')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
