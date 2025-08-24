9b2f6fe643f69e16d03d511bb6d9ced607677dbd
SujendraNayak <sujinayak008@gmail.com>
2025-08-24 14:29:05 +0530

    `Added job listings to home page and updated templates to display them`

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from flask import Flask, render_template
 app=Flask(__name__)
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'New York, NY',
+        'salary': '$120,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'San Francisco, CA',
+        'salary': '$150,000'
+    },
+    {
+        'id': 3,
+        'title': 'Product Manager',
+        'location': 'Austin, TX',
+        'salary': '$130,000'
+    }
+]
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',JOBS=JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
