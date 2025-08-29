789e33ae0614b8cb3e879260f49de7e0a8e9372c
SujendraNayak <sujinayak008@gmail.com>
2025-08-29 21:56:29 +0530

    `Added new route to display a specific job by ID and updated the app.py file`

--- app.py
+++ app.py
@@ -13,5 +13,13 @@ def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+    jobs = load_jobs_from_db()
+    job = next((job for job in jobs if job["id"] == id), None)
+    if job:
+        return render_template('jobpage.html', job=job)
+    return "Job not found", 404
+
 if __name__ == '__main__':
     app.run(debug=True)
